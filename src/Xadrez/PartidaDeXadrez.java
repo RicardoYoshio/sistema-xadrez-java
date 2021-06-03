89a2e9e91aa35d4750d8314c474899a71340c74a
RicardoYoshio <rica.tomokane@gmail.com>
2021-06-03 16:41:20 -0300

    Acrescentado pecas e colorindo

--- src/Xadrez/PartidaDeXadrez.java
+++ src/Xadrez/PartidaDeXadrez.java
@@ -2,7 +2,6 @@ package Xadrez;
 
 import Xadrez.peças.Rei;
 import Xadrez.peças.Torre;
-import tabuleiro.Posição;
 import tabuleiro.Tabuleiro;
 
 public class PartidaDeXadrez {
@@ -29,8 +28,18 @@ public class PartidaDeXadrez {
 	}
 	
 	private void setupInicial() {
-		colocarNovaPeça('b', 6, new Torre(tabuleiro, Cor.BRANCA));
-		colocarNovaPeça('e', 8, new Rei(tabuleiro, Cor.PRETA));
-		colocarNovaPeça('e', 1, new Rei(tabuleiro, Cor.BRANCA));
+		colocarNovaPeça('c', 1, new Torre(tabuleiro, Cor.BRANCA));
+		colocarNovaPeça('c', 2, new Torre(tabuleiro, Cor.BRANCA));
+		colocarNovaPeça('d', 2, new Torre(tabuleiro, Cor.BRANCA));
+		colocarNovaPeça('e', 2, new Torre(tabuleiro, Cor.BRANCA));
+		colocarNovaPeça('e', 1, new Torre(tabuleiro, Cor.BRANCA));
+		colocarNovaPeça('d', 1, new Rei(tabuleiro, Cor.BRANCA));
+
+		colocarNovaPeça('c', 7, new Torre(tabuleiro, Cor.PRETA));
+		colocarNovaPeça('c', 8, new Torre(tabuleiro, Cor.PRETA));
+		colocarNovaPeça('d', 7, new Torre(tabuleiro, Cor.PRETA));
+		colocarNovaPeça('e', 7, new Torre(tabuleiro, Cor.PRETA));
+		colocarNovaPeça('e', 8, new Torre(tabuleiro, Cor.PRETA));
+		colocarNovaPeça('d', 8, new Rei(tabuleiro, Cor.PRETA));
 	}
 }
